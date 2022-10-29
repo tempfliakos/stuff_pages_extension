246c7ec1134c6d0456dfabb2207e0c284e1a7722
akos.tempfli <tempfliakos@inf.elte.hu>
2022-10-29 11:22:42 +0200

    for xbox clean up some jQuery step 1

--- popup.js
+++ popup.js
@@ -1,84 +1,99 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if( request.message === "clicked_browser_action" ) {  
-          worker();
-      }
+    function (request, sender, sendResponse) {
+        if (request.message === "clicked_browser_action") {
+            worker();
+        }
     }
-  );
+);
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-async function worker(userId,gameId) {
-    for(let i = 0; i < 5; i++) {
-        window.scrollTo(0,document.body.scrollHeight/(5-i));
-        await sleep(500);
-    }
-    getTrophies(userId,gameId);
-    
 }
 
-function getTrophies(userId = 2,gameId) {    
-    const trophiesList = ($('ul.achilist')[0]).children;
-    let secretsList = [];
-    if($('ul.achilist.secret')[0]) {
-        secretsList = ($('ul.achilist.secret')[0]).children;
+async function worker(userId, gameId) {
+    for (let i = 0; i < 5; i++) {
+        window.scrollTo(0, document.body.scrollHeight / (5 - i));
+        await sleep(500);
     }
-    result = '';
-    let trophies = [];
+    getAchievements(userId, gameId);
+
+}
+
+function getAchievements(userId = 2, gameId) {
+    let achievementLists = document.getElementsByClassName('achilist');
+    let achievements = [];
     let secrets = [];
-    for(let trophy of trophiesList) {
-        if(trophy.children) {
-            let value = $(trophy).find('img')[1].src.split('trophy_')[1].split('.')[0];
-            value = value.charAt(0).toUpperCase() + value.slice(1);
-            trophies.push({
-                'picture': $(trophy).find('img')[0].src,
-                'title': trophy.children[1].children[0].innerText.trim(),
-                'description': trophy.children[1].children[1].innerText.trim(),
-                'value': value,
-            })
+    let result = [];
+    for (let achievementList of achievementLists) {
+        if (achievementList.classList.value.includes('secret')) {
+            secrets = prepareAchievements(achievementList.children);
+        } else {
+            achievements = prepareAchievements(achievementList.children);
         }
     }
 
-    for(let trophy of secretsList) {
-        if(trophy.children) {
-            let value = $(trophy).find('img')[1].src.split('trophy_')[1].split('.')[0];
-            value = value.charAt(0).toUpperCase() + value.slice(1);
-            secrets.push({
-                'picture': trophy.children[0].children[0].currentSrc,
-                'title': trophy.children[1].children[0].innerText.trim(),
-                'description': trophy.children[1].children[1].innerText.trim(),
-                'value': value,
-            })
-        }
-    }
-
-    for(let trophy of trophies) {
+    for (let trophy of achievements) {
         result += `insert into user_achievement(user_id, game_id, title, description, secret, picture, value, earned) values (${userId}, ${gameId}, '${replace(trophy.title)}', '${replace(trophy.description)}', false, '${trophy.picture}', '${trophy.value}', false);\n`;
     }
-    for(let trophy of secrets) {
+    for (let trophy of secrets) {
         result += `insert into user_achievement(user_id, game_id, title, description, secret, picture, value, earned) values (${userId}, ${gameId}, '${replace(trophy.title)}', '${replace(trophy.description)}', true, '${trophy.picture}', '${trophy.value}', false);\n`;
     }
-    download(`${document.title}.sql`,result);
+    download(`${document.title}.sql`, result);
+}
+
+function prepareAchievements(achievementList) {
+    let result = [];
+    if (achievementList) {
+        for (let achievement of achievementList) {
+            if (achievement.children) {
+                result.push(processAchievement(achievement.children));
+            }
+        }
+    }
+    return result;
+}
+
+function processAchievement(achievement) {
+    let img = achievement[0].children[0].currentSrc;
+    let data = achievement[1];
+    let title = data.children[0].children[0].children[0].innerHTML;
+    let value;
+    if (isXbox()) {
+        value = parseInt(data.children[0].children[1].children[1].children[0].innerHTML);
+    } else {
+        value = data.children[0].children[1].children[1].children[0].children[0].src.split('trophy_')[1].split('.')[0];
+        value = value.charAt(0).toUpperCase() + value.slice(1);
+    }
+
+    let description = data.children[1].innerHTML;
+    return {
+        'picture': img,
+        'title': title,
+        'description': description,
+        'value': value,
+    };
+}
+
+function isXbox() {
+    return window.location.href.includes('www.xboxachievements.com');
 }
 
 function replace(text) {
-    return text.replaceAll('\'','\'\'');
+    return text.replaceAll('\'', '\'\'');
 }
 
 function download(filename, text) {
     var pom = document.createElement("a");
     pom.setAttribute(
-    "href",
-    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+        "href",
+        "data:text/plain;charset=utf-8," + encodeURIComponent(text)
     );
     pom.setAttribute("download", filename);
     if (document.createEvent) {
-    var event = document.createEvent("MouseEvents");
-    event.initEvent("click", true, true);
-    pom.dispatchEvent(event);
+        var event = document.createEvent("MouseEvents");
+        event.initEvent("click", true, true);
+        pom.dispatchEvent(event);
     } else {
-    pom.click();
+        pom.click();
     }
 }
